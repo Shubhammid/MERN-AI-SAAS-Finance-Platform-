15e7416ae760959a025eece39ec1f075861e7551
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-03 14:40:17 +0530

    Add report processing job, email sending functionality, and report template generation

--- backend/src/cron/scheduler.ts
+++ backend/src/cron/scheduler.ts
@@ -1,5 +1,6 @@
 import cron from "node-cron";
 import { processRecurringTransactions } from "./jobs/transaction.job";
+import { processReportJob } from "./jobs/report.job";
 
 const scheduleJob = (name: string, time: string, job: Function) => {
   console.log(`Scheduling ${name} at ${time}`);
@@ -24,5 +25,6 @@ const scheduleJob = (name: string, time: string, job: Function) => {
 export const startJobs = () => {
   return [
     scheduleJob("Transactions", "5 0 * * *", processRecurringTransactions),
+    scheduleJob("Reports", "30 2 1 * *", processReportJob),
   ];
 };
--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -30,3 +30,7 @@ export function calculateNextOccurrence(
       return base;
   }
 }
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
